792649ba12c919ad74d0cc88dca9710ad0262b64
chaeli <s.kcl5243@gmail.com>
2022-07-15 16:44:48 +0900

    CHORE: update dashboard css

--- src/components/home/Home_Banner.js
+++ src/components/home/Home_Banner.js
@@ -6,15 +6,15 @@ function HomeBanner() {
 
     return (
         <div className={styles.ctn}>
-            <div className={styles.btitle}>학습 유형 테스트</div>
-                <div className={styles.btext}>나의 학습 유형이 궁금하다면? <br />
-                    나에게 맞는 공부법이 궁금하다면? <br />
-                    나에게 맞는 멘토와 스터디를 찾고 싶다면?</div>
+            <div className={styles.content}>
+                <div className={styles.btitle}>학습 유형 테스트</div>
+                <div className={styles.btext}>
+                    나의 학습 유형이 궁금하다면? <br />
+                    나에게 맞는 공부법이 궁금하다면?
+                </div>
                 <button className={styles.btntest}>테스트 하러가기 {">"}</button>
-                <img className={styles.imgFox} src={testfox} alt="여우" />
-            {/* <div className={styles.banner}>
-                
-            </div> */}
+            </div>
+            <img className={styles.imgFox} src={testfox} alt="여우" />
         </div>
     );
 }
