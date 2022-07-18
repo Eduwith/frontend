c474a50d3558551691d298e0624c4b2ff944c3d0
goeun208 <qop341@gmail.com>
2022-07-18 21:13:26 +0900

    CHORE: code update

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,28 +1,10 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import styles from "./Home.module.css";
 import fox from '../images/fox.png';
-import customAxios from '../customAxios';
 import Home_Study from "../components/home/Home_Study";
 import Mento from "../components/mentoring/Mento";
 
 function Home() {
-
-   // IP주소 변수 선언
-   const [ip, setIp] = useState('');
-
-   // IP주소 값을 설정합니다.
-   function callback(data) {
-     setIp(data);
-   }
- 
-   // 첫번째 렌더링을 다 마친 후 실행합니다.
-   useEffect(
-     () => {
-       // 클라이언트의 IP주소를 알아내는 백엔드의 함수를 호출합니다.
-       customAxios('/ip', callback);
-     }, []
-   );
-
   return (
     <div>
       <div className={styles.ctn}>
