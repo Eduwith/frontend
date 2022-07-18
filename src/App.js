987fd1693b8dc584739f5ac4278b8695156f0bb5
chaeli <s.kcl5243@gmail.com>
2022-07-19 02:49:02 +0900

    CHORE: volunteer props

--- src/App.js
+++ src/App.js
@@ -18,36 +18,37 @@ import MyPoint from './components/mypage/MyPoint';
 import vlists from './data.js';
 
 function App() {
-  //const [vlist, setVlist] = useState(vlists);
-  const [vlist, setVlist] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  // const apiVolunteer = "http://localhost:8080/api/volunteers";
-  const apiVolunteer = '';
-  const getVlist = async () => {
-    try {
-      // 요청이 시작 할 때에는 error 와 vlist 초기화 , loading 상태를 tru로
-      setError(null);
-      setVlist(null);
-      setLoading(true);
-      const response = await axios.get(apiVolunteer);
-      setVlist(response.data); // 데이터는 response.data 안에
-      console.log(response.data);
-    } catch (e) {
-      setError(e);
-      console.log(e);
-    }
-    setLoading(false);
-  };
+  const [vlist, setVlist] = useState(vlists);
 
-  useEffect(() => {
-    getVlist();
-  }, []);
+  // const [vlist, setVlist] = useState([]);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  // // const apiVolunteer = "http://localhost:8080/api/volunteers";
+  // const apiVolunteer = '';
+  // const getVlist = async () => {
+  //   try {
+  //     // 요청이 시작 할 때에는 error 와 vlist 초기화 , loading 상태를 tru로
+  //     setError(null);
+  //     setVlist(null);
+  //     setLoading(true);
+  //     const response = await axios.get(apiVolunteer);
+  //     setVlist(response.data); // 데이터는 response.data 안에
+  //     console.log(response.data);
+  //   } catch (e) {
+  //     setError(e);
+  //     console.log(e);
+  //   }
+  //   setLoading(false);
+  // };
 
-  if (loading) return <div>로딩중..</div>;
-  if (error) return <div>에러가 발생했습니다</div>;
-  // 아직 users가 받아와 지지 않았을 때는 아무것도 표시되지 않음
-  if (!vlist) return null;
+  // useEffect(() => {
+  //   getVlist();
+  // }, []);
+
+  // if (loading) return <div>로딩중..</div>;
+  // if (error) return <div>에러가 발생했습니다</div>;
+  // // 아직 users가 받아와 지지 않았을 때는 아무것도 표시되지 않음
+  // if (!vlist) return null;
 
 
 
@@ -55,12 +56,11 @@ function App() {
     <div className="App">
         <Router>
           <Routes>
-            <Route path="/" element={<Home />}>
-            </Route>
-            <Route path="/main" element={<Home />}>
-            </Route>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/main" element={<Home />}></Route>
             <Route path="/login" element={<Login />}></Route>
             <Route path="/join" element={<Join />}></Route>
+
             <Route path="/mypage" element={<MyPage/>}></Route>
             <Route path="/mymentoapply" element={<MyMentoApply/>}></Route>
             <Route path="/mymento" element={<MyMento/>}></Route>
@@ -70,7 +70,7 @@ function App() {
 
             <Route path="/mentoring" element={<Mentoring />}></Route>
             <Route path="/volunteer" element={<Volunteer/>}></Route>
-            <Route path="/volunteerdetail/:idex" element={<VolunteerDetail vlist={vlist}/>}></Route>
+            <Route path="/volunteerdetail/:idex" element={<VolunteerDetail />}></Route>
             
           </Routes>
         </Router>
--- src/routes/Volunteer.js
+++ src/routes/Volunteer.js
@@ -6,15 +6,13 @@ import Navbar from "../components/home/Navbar";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
-//import vlists from "../data";
+import vlists from "../data.js";
 
-//let vlists =[["오디오북 녹음하기", "서울 성북구", "2022.02.28~2022.03.06", "2022.03.07~2023.01.31", "5/10"], ["자료 번역하기", "경기도", "2022.02.28~2022.03.06", "2022.03.07~2023.01.31", "5/10"], ["디지털 취약계층을 위한 강의", "부산", "2022.02.28~2022.03.06", "2022.03.07~2023.01.31", "5/10"], ["강의 자막 업로드", "대한한국", "2022.02.28~2022.03.06", "2022.03.07~2023.01.31", "5/10"]];
 
-function Volunteer(props) {
-    //const [vlist, setVlist] = useState(vlists);
-
-    // const [vlists, setVlists] = useState([]);
-    // const getVlists = async () => {
+function Volunteer() {
+    const [vlist, setVlist] = useState(vlists);
+    // const [vlist, setVlist] = useState([]);
+    // const getVlist = async () => {
     //     const json= await(
     //         await fetch(
     //             'http://localhost:8080/volunteers'
@@ -25,35 +23,36 @@ function Volunteer(props) {
     // useEffect(() => {
     //     getVlists();
     // },[]);
-    const [vlists, setVlists] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-    // const apiVolunteer = "http://localhost:8080/api/volunteers";
-    const apiVolunteer = '';
-    const getVlists = async () => {
-        try {
-            // 요청이 시작 할 때에는 error 와 vlist 초기화 , loading 상태를 tru로
-            setError(null);
-            setVlists(null);
-            setLoading(true);
-            const response = await axios.get(apiVolunteer);
-            setVlists(response.data); // 데이터는 response.data 안에
-            console.log(response.data);
-        } catch (e) {
-            setError(e);
-            console.log(e);
-        }
-        setLoading(false);
-    };
 
-    useEffect(() => {
-        getVlists();
-    }, []);
+    // const [vlist, setVlist] = useState([]);
+    // const [loading, setLoading] = useState(false);
+    // const [error, setError] = useState(null);
+    // // const apiVolunteer = "http://localhost:8080/api/volunteers";
+    // const apiVolunteer = '';
+    // const getVlist = async () => {
+    //     try {
+    //         // 요청이 시작 할 때에는 error 와 vlist 초기화 , loading 상태를 tru로
+    //         setError(null);
+    //         setVlist(null);
+    //         setLoading(true);
+    //         const response = await axios.get(apiVolunteer);
+    //         setVlist(response.data); // 데이터는 response.data 안에
+    //         console.log(response.data);
+    //     } catch (e) {
+    //         setError(e);
+    //         console.log(e);
+    //     }
+    //     setLoading(false);
+    // };
 
-    if (loading) return <div>로딩중..</div>;
-    if (error) return <div>에러가 발생했습니다</div>;
-    // 아직 users가 받아와 지지 않았을 때는 아무것도 표시되지 않음
-    if (!vlists) return null;
+    // useEffect(() => {
+    //     getVlist();
+    // }, []);
+
+    // if (loading) return <div>로딩중..</div>;
+    // if (error) return <div>에러가 발생했습니다</div>;
+    // // 아직 users가 받아와 지지 않았을 때는 아무것도 표시되지 않음
+    // if (!vlist) return null;
 
 
     return (
@@ -70,17 +69,17 @@ function Volunteer(props) {
             <div className={styles.vlist}>,
                 <div>
                     <div className={styles.text}>목록보기 <hr /> </div>
-                    {vlists.map((item, idex) =>
+    
+                    {vlist.map((item, idex) =>
                     (
                         <div className={styles.listbox} key={idex}>
                             <div className={styles.boxtop}>
-                                <h4>{item[0]}</h4>
-                                <button>
-                                    <Link to={`/volunteerdetail/${idex}`
-                                    }>모집중</Link>
-                                </button>
+                                <h4>{vlist[idex].title}</h4>
+                                <Link to={`/volunteerdetail/${idex}`} state={{ data: vlist[idex] }}>
+                                    <button className={styles.recruitbtn}>모집중</button>
+                                </Link>
                             </div>
-                            <div className={styles.boxdetail}>[지역] {item[1]} &nbsp; [모집기간] {item[2]} ~ {item[3]}  &nbsp;  [봉사기간] {item[4]} ~ {item[5]} &nbsp;  [신청인원] {item[6]}/{item[7]}</div>
+                            <div className={styles.boxdetail}>[지역] {vlist[idex].address} &nbsp; [모집기간] {vlist[idex].r_start_date} ~ {vlist[idex].r_end_date}  &nbsp;  [봉사기간] {vlist[idex].v_start_date} ~ {vlist[idex].v_end_date} &nbsp;  [신청인원] {vlist[idex].current_people}/{vlist[idex].total_people}</div>
                            
                         </div>
                     ))}
